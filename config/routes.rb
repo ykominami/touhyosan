65d59d923215f65c87043740255dfd31fc1b04d2
ykominami <ykominami@gmail.com>
2023-03-08 22:05:34 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'polls#new'
+  # root 'polls#new'
+  root 'polls#index'
 
-  resources :polls, only: [:show, :new, :create] do
+  resources :polls, only: [:show, :new, :create, :index] do
     resources :votes, only: [:new, :create, :edit, :update], module: :polls
   end
 end
